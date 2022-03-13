ea85ac6251628af8b0f60e226901809f79a7f908
Tobias Tersigni <tobias.tersigni@live.de>
2022-03-13 14:44:11 +0100

    Adjust config to fix dev environment problems in VS Code

--- src/lib/header/SearchBar.svelte
+++ src/lib/header/SearchBar.svelte
@@ -1,27 +1,39 @@
-<!DOCTYPE html>
+<!DOCTYPE html />
 <html lang="en">
+    <head>
+        <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+            integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+            crossorigin="anonymous"
+            referrerpolicy="no-referrer"
+        />
+        <link
+            href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+            rel="stylesheet"
+        />
+    </head>
 
-<head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-</head>
-
-<body>
-    <h3 class="text-center mt-5">Tailwind Search Bar</h3>
-    <div class="mt-3 w-1/4 mr-auto ml-auto ">
-    <div class="px-2 flex items-center border-1 bg-white shadow-sm rounded-full">
-        <input class="rounded-l-sm w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none" id="search"
-                type="text" placeholder="Search">
-            <div class="p-2">
-                <button
-                    class="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
-                    <i class="fas fa-search"></i>
-                </button>
+    <body>
+        <h3 class="text-center mt-5">Tailwind Search Bar</h3>
+        <div class="mt-3 w-1/4 mr-auto ml-auto ">
+            <div
+                class="px-2 flex items-center border-1 bg-white shadow-sm rounded-full"
+            >
+                <input
+                    class="rounded-l-sm w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none"
+                    id="search"
+                    type="text"
+                    placeholder="Search"
+                />
+                <div class="p-2">
+                    <button
+                        class="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center"
+                    >
+                        <i class="fas fa-search" />
+                    </button>
+                </div>
             </div>
         </div>
-    </div>
-</body>
-
+    </body>
 </html>
--- src/routes/signup.svelte
+++ src/routes/signup.svelte
@@ -1,20 +1,31 @@
 <script lang="ts">
-        import SearchBar from '$lib/header/SearchBar.svelte';
+    import SearchBar from "$lib/header/SearchBar.svelte";
 </script>
+
 <svelte:head>
     <title>Create an account</title>
 </svelte:head>
 
 <h1>Create an account</h1>
 
-<SearchBar/>
+<SearchBar />
 
 <form method="POST" action="/signup">
     <fieldset>
         <label for="email">Email</label>
-        <input type="email" placeholder="user@email.net" name="email" required />
+        <input
+            type="email"
+            placeholder="user@email.net"
+            name="email"
+            required
+        />
         <label for="password">Password</label>
-        <input type="password" placeholder="Your password" name="password" required />
+        <input
+            type="password"
+            placeholder="Your password"
+            name="password"
+            required
+        />
         <label for="password">Password, again</label>
         <input
             type="password"
